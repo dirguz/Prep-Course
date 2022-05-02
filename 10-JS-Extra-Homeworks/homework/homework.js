65063355296edc1e0b98b4c1ee38d1b98e6319cc
Diego <diegoar.guz@hotmail.com>
2022-05-02 15:11:09 -0500

    avance homework extra

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,8 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var a=[];
+  return a=Object.entries(objeto);
 }
 
 
@@ -18,7 +20,20 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
-}
+  var resul={};
+  for (var i=0;i < string.length;i++) {
+    if(resul.hasOwnProperty(string[i])){
+      resul[string[i]] = resul[string[i]] + 1;
+    }
+    else{
+      resul[string[i]] = 1;
+    }
+    }
+    return resul;
+
+  }
+  
+
 
 
 function capToFront(s) {
